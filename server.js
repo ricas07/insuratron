966e4dde7f9065e4ac45ff9e69c849a111b63c27
Ricardo Perez <ricas07@gmail.com>
2018-12-24 15:41:23 -0800

    updated port

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const sendMail = require('./src/mail');
 const app = express();
 const upload = multer(); // for parsing multipart/form-data
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static('public')); // serving static files from /public
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -16,4 +18,4 @@ app.post('/submit-form', upload.array(), (req, res) => {
     sendMail(req.body);
     res.sendStatus(200);
 });
-app.listen(3000, () => console.log('This app is the shit!')); // eslint-disable-line no-console
+app.listen(port, () => console.log('This app is the shit!')); // eslint-disable-line no-console
